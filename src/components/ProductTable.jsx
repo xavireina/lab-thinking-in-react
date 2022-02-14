a17b41b8a787cc747ab9f0a730965279209c273c
xavireina <xavierreina93@gmail.com>
2022-02-14 19:42:12 +0100

    done

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,25 @@
+import ProductRow from './ProductRow';
+import './ProductTable.css';
+
+const ProductTable = ({products}) => {
+
+    return(
+        <table>
+            <thead>
+              <tr>
+               <th>Name</th>
+               <th>Price</th>
+              </tr>
+            </thead>
+
+            {products.map((product) => {
+                const { price, inStock, name, id } = product;
+                return (
+                    <ProductRow key={id} name={name} price={price} inStock={inStock} />
+                );
+            })}
+        </table>
+    )
+
+}
+export default ProductTable;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+
+
+class SearchBar extends Component{
+    state ={
+        input: ''
+    }
+
+    handleChange =(e) => {
+        this.props.filterProducts(e.target.value)
+        this.setState({
+            input: e.target.value
+        })
+    }
+
+    render () {
+
+        return <div>
+            <input
+                type='text'
+                className="input"
+                placeholder="Search..."
+                value={this.state.input}
+                onChange={this.handleChange}
+            />
+
+        </div>
+    }
+}
+
+export default SearchBar;
